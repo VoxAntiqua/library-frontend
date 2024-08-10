5c37407a38e87e7617ec6d9c7f31afe06e0f9036
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-10 12:24:37 -0400

    Implemented all books query

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,17 @@
-const Books = (props) => {
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../queries'
+
+const Books = props => {
   if (!props.show) {
     return null
   }
 
-  const books = []
+  const result = useQuery(ALL_BOOKS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  const books = result.data.allBooks
 
   return (
     <div>
@@ -16,7 +24,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books.map(a => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
