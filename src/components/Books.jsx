a29563c98af6522729a38806321a15f9b1e1c1a3
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-15 14:02:40 -0400

    Implemented genre filter using GraphQL query

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,22 +1,42 @@
+import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = props => {
-  const result = useQuery(ALL_BOOKS)
+  const [genreFilter, setGenreFilter] = useState(null)
+  const [uniqueGenres, setUniqueGenres] = useState([])
+  const allBooksResult = useQuery(ALL_BOOKS)
+
+  const filteredBooksResult = useQuery(ALL_BOOKS, {
+    variables: { genre: genreFilter },
+  })
+
+  useEffect(() => {
+    if (allBooksResult.data) {
+      const genres = allBooksResult.data.allBooks.flatMap(book => book.genres)
+      setUniqueGenres([...new Set(genres)])
+    }
+  }, [allBooksResult.data])
 
   if (!props.show) {
     return null
   }
 
-  if (result.loading) {
+  if (filteredBooksResult.loading) {
     return <div>loading...</div>
   }
-  const books = result.data.allBooks
+  const books = filteredBooksResult.data.allBooks
 
   return (
     <div>
       <h2>books</h2>
-
+      {genreFilter ? (
+        <>
+          in genre <b>{genreFilter}</b>
+        </>
+      ) : (
+        ''
+      )}
       <table>
         <tbody>
           <tr>
@@ -33,6 +53,23 @@ const Books = props => {
           ))}
         </tbody>
       </table>
+      {uniqueGenres.map(genre => (
+        <button
+          key={genre}
+          onClick={() => {
+            setGenreFilter(genre)
+          }}
+        >
+          {genre}
+        </button>
+      ))}
+      <button
+        onClick={() => {
+          setGenreFilter(null)
+        }}
+      >
+        all genres
+      </button>
     </div>
   )
 }
--- src/queries.js
+++ src/queries.js
@@ -12,13 +12,14 @@ export const ALL_AUTHORS = gql`
 `
 
 export const ALL_BOOKS = gql`
-  query {
-    allBooks {
+  query AllBooks($author: String, $genre: String) {
+    allBooks(author: $author, genre: $genre) {
       title
       author {
         name
       }
       published
+      genres
       id
     }
   }
