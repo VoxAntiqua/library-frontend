2c055fe6be10db173e362437e21f1e11b22c293c
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-14 20:53:53 -0400

    Added logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import Login from './components/Login'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   return (
     <div>
@@ -16,7 +24,7 @@ const App = () => {
         {token ? (
           <>
             <button onClick={() => setPage('add')}>add book</button>
-            <button>logout</button>
+            <button onClick={logout}>logout</button>
           </>
         ) : (
           <button onClick={() => setPage('login')}>login</button>
